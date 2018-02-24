9f2ddb5b55ea121eaf7cb778a1897769fe12d766
Alessandro Vecchi <aleangel1212@gmail.com>
2018-02-24 13:01:11 -0500

    Basic dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,9 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
-class Dashboard extends Component {
-	render() {
-		return <div className="container">Dashboard</div>;
+const Dashboard = props => {
+	function renderColumn(icon, amount, name) {
+		return (
+			<div className="column">
+				<div className="notification is-primary vd-box-shadow">
+					<div className="level is-mobile">
+						<div className="level-left">
+							<span className="icon is-large">
+								<i className={`fa fa-3x fa-${icon}`} />
+							</span>
+						</div>
+						<div className="level-right level-vertical">
+							<h2 className="title is-2">{amount}</h2>
+							<h4 className="subtitle is-4">{name}</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
+
+	function renderInfo(user) {
+		if (!user) return null;
+
+		if (user.type === 'employee' && props.clients) {
+			return renderColumn('briefcase', props.clients.length, 'Clients');
+		} else if (user.type === 'client' && props.clients) {
+			return renderColumn('wrench', props.employees.length, 'Technicians');
+		}
+
+		return null;
+	}
+
+	return (
+		<div>
+			<div className="columns is-marginless">
+				{props.reportsList
+					? renderColumn('clipboard', props.reportsList.length, 'Reports')
+					: null}
+				{renderInfo(props.me)}
+			</div>
+		</div>
+	);
+};
+
+function mapStateToProps(state) {
+	return {
+		me: state.users.me,
+		reportsList: state.reports.list,
+		clients: state.users.clients,
+		employees: state.users.employees,
+	};
 }
 
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
