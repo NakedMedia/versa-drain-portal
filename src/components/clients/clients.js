f0cf638a98c2fd0a4e369939d0af9b414fb63e80
Alessandro Vecchi <aleangel1212@gmail.com>
2018-03-08 15:51:37 -0500

    Client search

--- src/components/clients/clients.js
+++ src/components/clients/clients.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -8,8 +8,20 @@ import * as actions from '../../actions';
 
 import emptyProfile from '../../img/empty-profile.jpg';
 
-const Clients = props => {
-	function renderAdminOptions(type, client) {
+class Clients extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			search: '',
+		};
+	}
+
+	handleSearch(e) {
+		this.setState({ search: e.target.value.toLowerCase() });
+	}
+
+	renderAdminOptions(type, client) {
 		if (type !== 'admin') return null;
 
 		return (
@@ -23,7 +35,7 @@ const Clients = props => {
 				<span className="icon has-text-primary is-large">
 					<a
 						onClick={() => {
-							props.deleteUser(client);
+							this.props.deleteUser(client);
 						}}
 					>
 						<i className="fas fa-trash" />
@@ -33,66 +45,97 @@ const Clients = props => {
 		);
 	}
 
-	function renderClients(clients) {
-		if (!props.clientsList || !props.me) return <div className="loader" />;
-		if (props.clientsList.length === 0) return <h3>No Clients</h3>;
+	renderClients(clients) {
+		if (!this.props.clientsList || !this.props.me) return <div className="loader" />;
+		if (this.props.clientsList.length === 0) return <h3>No Clients</h3>;
 
-		return clients.map(client => (
-			<div className="box" key={client.id}>
-				<article className="media">
-					<div className="media-left">
-						<figure className="image is-64x64 vd-profile-picture">
-							<img src={client.img || emptyProfile} alt="Client" />
-						</figure>
-					</div>
-					<div className="media-content">
-						<div className="content">
-							<p>
-								<Link to={`${routes.webRoot}/reports/${client.id}`}>
-									{client.name}
-								</Link>
-								<br />
-								<small>
-									<strong>Client ID: </strong>
-									{client.id}
-								</small>
-								<br />
-								<small>
-									<strong>Address: </strong>
-									{client.address ? (
-										<a href={`https://maps.google.com/?q=${client.address}`}>
-											{client.address}
-										</a>
-									) : (
-										'N/A'
-									)}
-								</small>
-								<br />
-								<small>
-									<strong>Phone: </strong>
-									{client.contact_phone || 'N/A'}
-								</small>
-								<br />
-								<small>
-									<strong>Contact Name: </strong>
-									{client.contact_name || 'N/A'}
-								</small>
-								<br />
-								<small>
-									<strong>Contact Email: </strong>
-									{client.contact_email || 'N/A'}
-								</small>
-							</p>
+		return clients
+			.filter(
+				client =>
+					client.id.toString().startsWith(this.state.search) ||
+					client.name.toLowerCase().startsWith(this.state.search) ||
+					client.address.toLowerCase().startsWith(this.state.search) ||
+					client.contact_name.toLowerCase().startsWith(this.state.search) ||
+					client.contact_email.toLowerCase().startsWith(this.state.search) ||
+					client.contact_phone.toLowerCase().startsWith(this.state.search)
+			)
+			.map(client => (
+				<div className="box" key={client.id}>
+					<article className="media">
+						<div className="media-left">
+							<figure className="image is-64x64 vd-profile-picture">
+								<img src={client.img || emptyProfile} alt="Client" />
+							</figure>
 						</div>
-					</div>
-					{renderAdminOptions(props.me.type, client)}
-				</article>
-			</div>
-		));
+						<div className="media-content">
+							<div className="content">
+								<p>
+									<Link to={`${routes.webRoot}/reports/${client.id}`}>
+										{client.name}
+									</Link>
+									<br />
+									<small>
+										<strong>Client ID: </strong>
+										{client.id}
+									</small>
+									<br />
+									<small>
+										<strong>Address: </strong>
+										{client.address ? (
+											<a
+												href={`https://maps.google.com/?q=${client.address}`}
+											>
+												{client.address}
+											</a>
+										) : (
+											'N/A'
+										)}
+									</small>
+									<br />
+									<small>
+										<strong>Phone: </strong>
+										{client.contact_phone || 'N/A'}
+									</small>
+									<br />
+									<small>
+										<strong>Contact Name: </strong>
+										{client.contact_name || 'N/A'}
+									</small>
+									<br />
+									<small>
+										<strong>Contact Email: </strong>
+										{client.contact_email || 'N/A'}
+									</small>
+								</p>
+							</div>
+						</div>
+						{this.renderAdminOptions(this.props.me.type, client)}
+					</article>
+				</div>
+			));
 	}
 
-	return <div>{renderClients(props.clientsList)}</div>;
-};
+	render() {
+		return (
+			<div>
+				<div className="field has-addons">
+					<p className="control has-icons-left has-icons-right vd-report-search">
+						<input
+							className="input"
+							type="text"
+							placeholder="Search Clients"
+							onChange={this.handleSearch.bind(this)}
+						/>
+						<span className="icon is-small is-left">
+							<i className="fas fa-search" />
+						</span>
+					</p>
+				</div>
+				{this.renderClients(this.props.clientsList)}
+			</div>
+		);
+	}
+}
 
 function mapStateToProps(state) {
 	return {
